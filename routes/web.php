e7e69a9bc938b9096143da4937948c7b98dcd6fc
Demetri Samartsev <samartsew@gmail.com>
2022-09-18 07:34:07 +0300

    del middleware not needed

--- routes/web.php
+++ routes/web.php
@@ -32,22 +32,22 @@ Route::group([
 
         //Категории
         Route::get('/categories', [Controllers\CategoryController::class, 'index'])->name('category.index');
-        Route::get('/categories/create', [Controllers\CategoryController::class, 'create'])->middleware(['auth'])->name('category.create');
+        Route::get('/categories/create', [Controllers\CategoryController::class, 'create'])->name('category.create');
         Route::post('/categories', [Controllers\CategoryController::class, 'store'])->name('category.store');
-        Route::get('/categories/{id}/edit', [Controllers\CategoryController::class, 'edit'])->middleware(['auth'])->name('category.edit');
+        Route::get('/categories/{id}/edit', [Controllers\CategoryController::class, 'edit'])->name('category.edit');
         Route::patch('/categories/{id}', [Controllers\CategoryController::class, 'update'])->name('category.update');
 
-        Route::get('/categories/{id}/delete', [Controllers\CategoryController::class, 'delete'])->middleware(['auth'])->name('category.delete');
+        Route::get('/categories/{id}/delete', [Controllers\CategoryController::class, 'delete'])->name('category.delete');
         Route::delete('/categories/{id}', [Controllers\CategoryController::class, 'destroy'])->name('category.destroy');
 
         //Коллекции
         Route::get('/pr_collections', [Controllers\PrCollectionController::class, 'index'])->name('pr_collection.index');
-        Route::get('/pr_collections/create', [Controllers\PrCollectionController::class, 'create'])->middleware(['auth'])->name('pr_collection.create');
+        Route::get('/pr_collections/create', [Controllers\PrCollectionController::class, 'create'])->name('pr_collection.create');
         Route::post('/pr_collections', [Controllers\PrCollectionController::class, 'store'])->name('pr_collection.store');
 
         //Цвета 
         Route::get('/pr_cvets', [Controllers\PrCvetController::class, 'index'])->name('pr_cvet.index');
-        Route::get('/pr_cvets/create', [Controllers\PrCvetController::class, 'create'])->middleware(['auth'])->name('pr_cvet.create');
+        Route::get('/pr_cvets/create', [Controllers\PrCvetController::class, 'create'])->name('pr_cvet.create');
         Route::post('/pr_cvets', [Controllers\PrCvetController::class, 'store'])->name('pr_cvet.store');
         Route::get('/pr_cvets/{id}', [Controllers\PrCvetController::class, 'show'])->name('pr_cvet.show');
 
