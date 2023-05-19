9bcbff8863fd4b2d5c05c55d00b26ee2ef12941c
Demetri Samartsev <samartsew@gmail.com>
2023-05-19 13:12:18 +0300

    poliiamid-desc

--- resources/views/selection.blade.php
+++ resources/views/selection.blade.php
@@ -6,11 +6,13 @@
         {{ $description }}
     </x-slot:description>
     <h1 style="text-align: center">{{ $title }}</h1>
+    <br>
     <section class="recomendations">
         <x-public.nodes :products="$products"/>
         <div class="recomendations__paginator">
             {{ $products->links('vendor.pagination.default') }}
         </div>
     </section>
+    <br>
     <article><p>{{ $description }}</p></article>
 </x-layout>
