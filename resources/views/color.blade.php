f0f0d8e0da3be832756a031b148d4cbe5f0ab00c
Demetri Samartsev <samartsew@gmail.com>
2023-05-22 16:52:33 +0300

    fix-color-title

--- resources/views/color.blade.php
+++ resources/views/color.blade.php
@@ -1,12 +1,12 @@
 <x-layout>
     <x-slot:title>
-        {{ __('public.colors.' . $color->slug . '.title') }}
+        {{ __('public.colors.' . $category->slug . '.' . $color->slug . '.title') }}
     </x-slot:title>
     <x-slot:description>
         {{ __('public.colors.' . $category->slug . '.' . $color->slug . '.description') }}
     </x-slot:description>
     <section class="recomendations">
-        <x-public.nodes :title="__('public.colors.' . $category->slug . '.' . $color->slug . '.title')" :products="$products" />
+        <x-public.nodes :title="__('public.colors.' . $category->slug . '.' . $color->slug . '.h1')" :products="$products" />
         <div class="recomendations__paginator">
             {{ $products->links('vendor.pagination.default') }}
         </div>
