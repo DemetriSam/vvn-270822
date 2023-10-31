0e20d1ee9eb66e17168fc8e83a18ec7452661d4e
Demetri Samartsev <samartsew@gmail.com>
2023-10-31 20:28:02 +0300

    article-css-fix

--- resources/views/selection.blade.php
+++ resources/views/selection.blade.php
@@ -7,14 +7,6 @@
     </x-slot:description>
     <h1 style="text-align: center">{{ $h1 }}</h1>
     <div class="decorline"><img src="../../../img/icons/DecorLine.svg" alt=""></div>
-    <style>
-        div.decorline {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            margin: 20px auto;
-        }
-    </style>
     <section class="recomendations">
         <x-public.nodes :products="$products"/>
         <div class="recomendations__paginator">
@@ -22,24 +14,11 @@
         </div>
     </section>
     <br>
-    <article>{!! $text_content !!}</article>
-    <style>
-        article {
-            max-width: 574px;
-            margin: 0 auto;
+    @php
+        $pageN = Request::input('pageN');
+    @endphp
+    @if (!isset($pageN))
+        <article>{!! $text_content !!}</article>
+    @endif
 
-        }
-        article ul {
-            padding-left: 1em;
-        }
-        article p, article ul {
-            margin: 0 0 1em 0;
-            line-height: 1.5;
-        }
-
-        article ul li {
-            margin: 0 0 0.3em 0;
-            list-style-type: circle;
-        }
-    </style>
 </x-layout>
