b0e559bd339c4b685c4ceb85e01649b61c180f1f
Demetri Samartsev <samartsew@gmail.com>
2023-11-12 13:49:25 +0300

    fix-favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <title>{{ $title }}</title>
     <meta charset="UTF-8">
     <meta name="format-detection" content="telephone=no">
-    <link rel="shortcut icon" href="favicon.svg">
+    <link rel="shortcut icon" href="/favicon.svg">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="{{ $description }}">
     <script type="module" src="/swiper-bundle.min.js"></script>
