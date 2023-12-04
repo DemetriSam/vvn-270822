25cbeacda12014d4311c705070f8853a5f852d91
DemetriSam <samartsew@gmail.com>
2023-12-04 10:00:23 +0300

    fix-prev-blog

--- resources/views/components/blog-article.blade.php
+++ resources/views/components/blog-article.blade.php
@@ -2,13 +2,7 @@
     $media = $post->getFirstMedia('blog');
 @endphp
 <div class="artann anns__ann">
-    <a href="{{ route('page', ['page' => $post->slug]) }}"><div class="artann__pic">
-        @if(isset($media))
-            {{ $media('tile') }}
-        @else
-            <img src="/img/gallery/DG 8002 hor.jpg" />
-        @endif
-    </div></a>
+    <a href="{{ route('page', ['page' => $post->slug]) }}"><div class="artann__pic">{{ $post->getFirstMedia('blog')('tile') }}</div></a>
     {{-- <div class="artann__tag article-tag">Ремонт</div> --}}
     <div class="artann__label"><a href="{{ route('page', ['page' => $post->slug]) }}">{{$post->title}}</a></div>
     <div class="artann__text">{{ $post->ann }}</div>
